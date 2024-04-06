2f5f170f343fee06594dcd5857efde68548e31ab
sandeep vemula <sandeepvemula408@gmail.com>
2024-04-06 17:23:55 +0530

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import World from '../hero/World'
 
 const Hero = () => {
+    const [width, setWidth] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth > 1024);
+        };
+        window.addEventListener("resize", handleResize);
+        handleResize();
+    });
     return (
-        <div className='py-52  lg:h-screen w-full lg:max-w-7xl mx-auto  relative text-white font-bold  tracking-wider text-center ' >
-            <h1 className='font-cabinet font-black text-[44px] lg:text-[100px]'>Hi,I'm Sandeep,</h1>
-            <p className='font-cabinet font-black  text-[44px] lg:text-[100px]'>Front End Web Developer</p>
-            {/* <World /> */}
-        </div>
+        <>
+            {
+                width ? (<div className='lg:h-screen w-full  mx-auto text-white font-bold' >
+
+                    <World />
+                </div>) : (
+                    <div className='flex flex-col items-center justify-center  h-screen w-full  mx-auto  relative text-white font-bold  tracking-wider text-center ' >
+                        <h1 className='font-cabinet font-black text-[44px] lg:text-[100px]'>Hi,I'm Sandeep,</h1>
+                        <p className='font-cabinet font-black  text-[44px] lg:text-[100px]'>Front End Web Developer</p>
+                    </div>
+                )
+            }
+        </>
+
     )
 }
 
--- src/skills/World.jsx
+++ src/skills/World.jsx
@@ -1,17 +1,27 @@
 import { Canvas } from '@react-three/fiber'
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import Experience from './Experience'
 import { TrackballControls } from '@react-three/drei'
 
 
 const World = () => {
+    const [width, setWidth] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth > 1024);
+        };
+        window.addEventListener("resize", handleResize);
+        handleResize();
+    });
+
     return (
         <div className='w-full h-[500px] lg:w-full lg:h-[600px] z-40'>
             <Canvas camera={{ position: [0, 0, 47.5], fov: 95 }}>
                 <Suspense>
                     <Experience />
                 </Suspense>
-                <TrackballControls noZoom />
+                {width ? (<TrackballControls noZoom />) : ("")}
             </Canvas>
         </div>
     )
